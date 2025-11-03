10b7e96bcc5b6323421cf3529a14141c19e0a142
tilliedstrom <tilliedstrom@elev.ga.ntig.se>
2025-11-03 10:21:03 +0100

    replace code

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-// const playerName = prompt("Skriv ditt namn: ")
-const playerName = "Jens" // statiskt så vi slipper prompt varje körning
-let playerHp = 100
-let enemyHp = 100
+const playerName = "Perry the scragulet knight"
+let playerHP = 20
+let enemyHP = 10
+playerRoll = rollDice(6)
+enemyRoll = rollDice(6)
 
-function rollDice() {
-    return Math.ceil(Math.random() * 6)
+
+function rollDice(diceType) {
+    return Math.ceil(Math.random() * diceType)
 }
 
 
-console.log(rollDice())
-
-let playerRoll = rollDice()
-let enemyRoll = rollDice()
-
-function gameRound() {
+function turn() {
+    console.log(`enmey ` + enemyRoll)
+    console.log(`player ` + playerRoll)
     if (playerRoll > enemyRoll) {
-        const damage = playerRoll - enemyRoll
-        console.log(`Du köttar fienden för ${damage}!`)
-        enemyHp -= damage
+        enemyHP -= playerRoll
+        console.log(`enemy ` + enemyHP)
     } else if (enemyRoll > playerRoll) {
-        const damage = enemyRoll - playerRoll
-        console.log(`Nedrans, du blir mulad för ${damage}!`)
-        playerHp -= damage
+        playerHP -= enemyHP
+        console.log(`player ` + playerHP)
     } else {
-        console.log("Snyggt parerat, inget händer!")
+        const dmg = (playerRoll + enemyRoll)/2
+        playerHP -= dmg
+        enemyHP -= dmg
+        console.log(`enemy ` + enemyHP + ` player ` + playerHP )
     }
 }
 
-gameRound()
+
+while (playerHP > 0 && enemyHP > 0) {
+    turn(playerRoll, enemyRoll)
+}
